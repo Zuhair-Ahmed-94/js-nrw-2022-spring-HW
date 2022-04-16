427b141ff1681e882a7abcd997f070c1ba27e8e7
Latif Turaan <88229875+19latif@users.noreply.github.com>
2022-04-16 12:06:07 +0200

    Add files via upload
    Homework

---
+++ Abdul Latif/dom.js
@@ -0,0 +1,13 @@
+// const menuClicked = (curEl) => {
+//     const menuItems = document.getElementsByClassName('menu-item');
+
+//     for (let i = 0; i < menuItems.length; i++) {
+//         menuItems[i].classList.remove('active');
+//     }
+
+//     curEl.classList.add('active');
+// }
+
+function changeColor() {
+  document.querySelector('.personal_info').style.color = 'green';
+}
